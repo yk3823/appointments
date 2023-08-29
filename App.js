75b1bbc41a1f3b608c87f65fdea8e33a6dcdd303
Joseph Keinan <yk3823@gmail.com>
2023-08-29 16:36:00 +0300

    adding new code

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Platform } from 'react-native';
+import { useDimensions } from '@react-native-community/hooks'
 
 export default function App() {
+  console.log(useDimensions());
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text> Hello React Native</Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image
+          fadeDuration={1000} //מעלה את התמונה בהדרגה
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }} />
+      </TouchableOpacity>
+      <Button
+        title="Click Me"
+        onPress={() => Alert.alert("My title", "My message", [
+          { text: "Yes", onPress: () => console.log("Yes") },
+          { text: "No", onPress: () => console.log("No") },])}
+      ></Button>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
