cc0d8e3c0e20f7c73d10be6fa1b7c7698de623cc
Joseph Keinan <yk3823@gmail.com>
2023-08-29 18:31:02 +0300

    building the welcome screen

--- App.js
+++ App.js
@@ -1,37 +1,13 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Platform } from 'react-native';
-import { useDimensions } from '@react-native-community/hooks'
+import { View } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
 
 export default function App() {
-  console.log(useDimensions());
   return (
-    <SafeAreaView style={styles.container}>
-      <Text> Hello React Native</Text>
-      <TouchableOpacity onPress={() => console.log("Image tapped")}>
-        <Image
-          fadeDuration={1000} //מעלה את התמונה בהדרגה
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300"
-          }} />
-      </TouchableOpacity>
-      <Button
-        title="Click Me"
-        onPress={() => Alert.alert("My title", "My message", [
-          { text: "Yes", onPress: () => console.log("Yes") },
-          { text: "No", onPress: () => console.log("No") },])}
-      ></Button>
-      {/* <StatusBar style="auto" /> */}
-    </SafeAreaView>
+    <WelcomeScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
