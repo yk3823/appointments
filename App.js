a714ac206e1880217cf0d4d9d11cef6f2b2f119c
Joseph Keinan <yk3823@gmail.com>
2023-09-27 17:02:55 +0300

    Native Features

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import React from "react";
-// import AccountScreen from "./app/screens/AccountScreen";
-// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-// import ListingEditScreen from "./app/screens/ListingEditScreen";
-// import ListingsScreen from "./app/screens/ListingsScreen";
-// import LoginScreen from "./app/screens/LoginScreen";
-// import MessagesScreen from "./app/screens/MessagesScreen";
-// import RegisterScreen from "./app/screens/RegisterScreen";
-// import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <GestureHandlerRootView style={{ flex: 1 }}><WelcomeScreen /></GestureHandlerRootView>;
+  return <ListingEditScreen />;
 }
