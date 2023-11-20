dc7aeed13e265a7f1f6d54ec739af26ae2088b92
Joseph Keinan <yk3823@gmail.com>
2023-11-20 12:06:12 +0200

    replacing mode to presentation in FeedNavigator.js

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -6,7 +6,7 @@ import ListingDetailsScreen from "../screens/ListingDetailsScreen";
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-    <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator presentation="modal" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Listings" component={ListingsScreen} />
         <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
     </Stack.Navigator>
